32af968032afaea34841eabdb7beffe0e524138e
HugoHtvl <hug.oo@hotmail.fr>
2023-06-05 14:32:26 +0200

    modification css

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,15 +3,11 @@ import "./Filter.css";
 const Filter = ({ handleFilter, handleReset }) => {
   return (
     <>
-      <div className="divButtons">
-        <div>
-          <h3>Choose your ranger sex :</h3>
-        </div>
-        <div className="buttons">
-          <button onClick={() => handleFilter("male")}>Male</button>
-          <button onClick={() => handleFilter("female")}>Female</button>
-          <button onClick={() => handleFilter("machine")}>Machine</button>
-        </div>
+      <h3 id="h3Filter">Choose your ranger sex :</h3>
+      <div className="buttons">
+        <button onClick={() => handleFilter("male")}>Male</button>
+        <button onClick={() => handleFilter("female")}>Female</button>
+        <button onClick={() => handleFilter("machine")}>Machine</button>
       </div>
       <div className="reset">
         <button onClick={() => handleReset("rest")}>Reset</button>
